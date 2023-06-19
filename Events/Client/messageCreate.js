ebacfc628af7bf287bf01304b6b8326a790bc2d7
sharpenhead <119821328+sharpenhead@users.noreply.github.com>
2023-06-18 22:22:42 -0400

    Update messageCreate.js

--- Events/Client/messageCreate.js
+++ Events/Client/messageCreate.js
@@ -5,6 +5,7 @@ const {
     ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
+    ComponentType,
 } = require("discord.js");
 const linkSchema = require("../../Models/antilink");
 const antilinkLogSchema = require("../../Models/antilinkLogChannel");
@@ -21,7 +22,7 @@ module.exports = {
 
         let requireDB = await linkSchema.findOne({ _id: msg.guild.id });
         const data = await antilinkLogSchema.findOne({ Guild: msg.guild.id });
-
+        if (!data) return;
         if (!requireDB) return;
 
         if (requireDB.logs === false) return;
@@ -40,6 +41,7 @@ module.exports = {
                     .setDescription(`:warning: | Links are not allowed in this server, ${user}.`)
                     .setColor(warningColor);
 
+                /*
                 const url =
                     /((([(https)(http)]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
                 // Créditos pela regex: Tech Boy
@@ -54,143 +56,156 @@ module.exports = {
                         return;
                     }
                 }, 2000); // Coloquei um limite de tempo pra evitar ratelimit
+                */
 
-                const logChannel = client.channels.cache.get(data.logChannel)
+                const linkRegex = /(https?:\/\/[^\s]+|discord\.gg\/[^\s]+)/gi; // Regular expression to match URLs and discord.gg/ links
 
-                if (!logChannel) return;
-                else {
-                    const buttons = new ActionRowBuilder()
-                        .addComponents(
-                            new ButtonBuilder()
-                                .setLabel("Timeout")
-                                .setEmoji("🔨")
-                                .setCustomId("linktimeout")
-                                .setStyle(ButtonStyle.Secondary),
-                            new ButtonBuilder()
-                                .setLabel("Kick")
-                                .setEmoji("🛠️")
-                                .setCustomId("linkkick")
-                                .setStyle(ButtonStyle.Danger)
-                        );
+                const content = msg.content.toLowerCase();
+                const words = content.split(' ');
 
-                    // For sending message to log channel.
-                    const text = await logChannel.send({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setColor(mainColor)
-                                .setDescription(`<@${user.id}> has been warned for sending a link.\n\`\`\`${msg.content}\`\`\``)
-                                .setFooter({ text: `User ID: ${user.id}` })
-                                .setTimestamp()
-                        ],
-                        components: [buttons]
-                    });
+                for (const word of words) {
+                    if (linkRegex.test(word)) {
+                        msg.delete();
+                        const logChannel = client.channels.cache.get(data.logChannel);
 
-                    const col = await text.createMessageComponentCollector();
+                        if (!logChannel) return;
+                        else {
+                            const buttons = new ActionRowBuilder()
+                                .addComponents(
+                                    new ButtonBuilder()
+                                        .setLabel("Timeout")
+                                        .setEmoji("🔨")
+                                        .setCustomId("linktimeout")
+                                        .setStyle(ButtonStyle.Secondary),
+                                    new ButtonBuilder()
+                                        .setLabel("Kick")
+                                        .setEmoji("🛠️")
+                                        .setCustomId("linkkick")
+                                        .setStyle(ButtonStyle.Danger)
+                                );
 
-                    col.on("collect", async (m) => {
-                        switch (m.customId) {
-                            case "linktimeout": {
-                                if (!m.member.permissions.has(PermissionFlagsBits.ModerateMembers))
-                                    return m.reply({
-                                        embeds: [
-                                            new EmbedBuilder()
-                                                .setColor(warningColor)
-                                                .setDescription(`:warning: | ${m.user.name} is missing the *moderate_members* permission, please try again after you gain this permission.`)
-                                        ],
-                                        ephemeral: true,
-                                    });
+                            // For sending message to log channel.
+                            const logMsg = await logChannel.send({
+                                embeds: [
+                                    new EmbedBuilder()
+                                        .setColor(mainColor)
+                                        .setDescription(`<@${user.id}> has been warned for sending a link.\n\`\`\`${msg.content}\`\`\``)
+                                        .setFooter({ text: `User ID: ${user.id}` })
+                                        .setTimestamp()
+                                ],
+                                components: [buttons]
+                            });
 
-                                if (!msg.member) {
-                                    return m.reply({
-                                        embeds: [
-                                            new EmbedBuilder()
-                                                .setDescription(`:warning: | The target specified has most likely left the server.`)
-                                                .setColor(warningColor)
-                                        ],
-                                        ephemeral: true,
-                                    });
+                            const col = await logMsg.createMessageComponentCollector({
+                                componentType: ComponentType.Button,
+                            });
+
+                            col.on("collect", async (m) => {
+                                switch (m.customId) {
+                                    case "linktimeout": {
+                                        if (!m.member.permissions.has(PermissionFlagsBits.ModerateMembers))
+                                            return m.reply({
+                                                embeds: [
+                                                    new EmbedBuilder()
+                                                        .setColor(warningColor)
+                                                        .setDescription(`:warning: | ${m.user} is missing the *moderate_members* permission, please try again after you gain this permission.`)
+                                                ],
+                                                ephemeral: true,
+                                            });
+
+                                        if (!msg.member) {
+                                            return m.reply({
+                                                embeds: [
+                                                    new EmbedBuilder()
+                                                        .setDescription(`:warning: | The target specified has most likely left the server.`)
+                                                        .setColor(warningColor)
+                                                ],
+                                                ephemeral: true,
+                                            });
+                                        }
+
+                                        m.reply({
+                                            embeds: [
+                                                new EmbedBuilder()
+                                                    .setColor(successColor)
+                                                    .setDescription(`:white_check_mark: | ${msg.member} has been successfully timed out for 10 minutes.`)
+                                            ],
+                                            ephemeral: true,
+                                        });
+
+                                        const timeoutEmbed = new EmbedBuilder()
+                                            .setTitle("Timeout")
+                                            .setDescription(
+                                                `:warning: | You have received a timeout from \`${msg.guild.name}\` for sending links.`
+                                            )
+                                            .setTimestamp()
+                                            .setColor(warningColor);
+
+                                        msg.member
+                                            .send({
+                                                embeds: [timeoutEmbed],
+                                            })
+                                            .then(() => {
+                                                const time = ms("10m");
+                                                msg.member.timeout(time);
+                                            });
+                                    }
+                                        break;
+
+                                    case "linkkick": {
+                                        if (!m.member.permissions.has(PermissionFlagsBits.KickMembers))
+                                            return m.reply({
+                                                embeds: [
+                                                    new EmbedBuilder()
+                                                        .setColor(warningColor)
+                                                        .setDescription(`:warning: | ${m.user} is missing the *kick_members* permission, please try again after you gain this permission.`)
+                                                ],
+                                                ephemeral: true,
+                                            });
+
+                                        const kickEmbed = new EmbedBuilder()
+                                            .setTitle("Kicked")
+                                            .setDescription(
+                                                `:warning: | You have been kicked from \`${msg.guild.name}\` for sending links.`
+                                            )
+                                            .setTimestamp()
+                                            .setColor(warningColor);
+
+                                        if (!msg.member) {
+                                            return m.reply({
+                                                embeds: [
+                                                    new EmbedBuilder()
+                                                        .setDescription(`:warning: | The target specified has most likely left the server.`)
+                                                        .setColor(warningColor)
+                                                ],
+                                                ephemeral: true,
+                                            });
+                                        }
+
+                                        m.reply({
+                                            embeds: [
+                                                new EmbedBuilder()
+                                                    .setColor(successColor)
+                                                    .setDescription(`:white_check_mark: | ${msg.member} has been successfully kicked from the server.`)
+                                            ],
+                                            ephemeral: true,
+                                        });
+
+                                        msg.member
+                                            .send({
+                                                embeds: [kickEmbed],
+                                            })
+                                            .then(() => {
+                                                msg.member.kick({ reason: "Sending links." });
+                                            });
+                                    }
+                                        break;
                                 }
-
-                                m.reply({
-                                    embeds: [
-                                        new EmbedBuilder()
-                                            .setColor(successColor)
-                                            .setDescription(`:white_check_mark: | ${msg.member} has been successfully timed out for 10 minutes.`)
-                                    ],
-                                    ephemeral: true,
-                                });
-
-                                const timeoutEmbed = new EmbedBuilder()
-                                    .setTitle("Timeout")
-                                    .setDescription(
-                                        `You have received a timeout from \`${msg.guild.name}\` for sending links.`
-                                    )
-                                    .setTimestamp()
-                                    .setColor(warningColor);
-
-                                msg.member
-                                    .send({
-                                        embeds: [timeoutEmbed],
-                                    })
-                                    .then(() => {
-                                        const time = ms("10m");
-                                        msg.member.timeout(time);
-                                    });
-                            }
-                                break;
-
-                            case "linkkick": {
-                                if (!m.member.permissions.has(PermissionFlagsBits.KickMembers))
-                                    return m.reply({
-                                        embeds: [
-                                            new EmbedBuilder()
-                                                .setColor(warningColor)
-                                                .setDescription(`:warning: | ${m.user.name} is missing the *kick_members* permission, please try again after you gain this permission.`)
-                                        ],
-                                        ephemeral: true,
-                                    });
-
-                                const kickEmbed = new EmbedBuilder()
-                                    .setTitle("Kicked")
-                                    .setDescription(
-                                        `:warning: | You have been kicked from \`${msg.guild.name}\` for sending links.`
-                                    )
-                                    .setTimestamp()
-                                    .setColor(warningColor);
-
-                                if (!msg.member) {
-                                    return m.reply({
-                                        embeds: [
-                                            new EmbedBuilder()
-                                                .setDescription(`:warning: | The target specified has most likely left the server.`)
-                                                .setColor(warningColor)
-                                        ],
-                                        ephemeral: true,
-                                    });
-                                }
-
-                                m.reply({
-                                    embeds: [
-                                        new EmbedBuilder()
-                                            .setColor(successColor)
-                                            .setDescription(`:white_check_mark: | ${msg.member} has been successfully kicked from the server.`)
-                                    ],
-                                    ephemeral: true,
-                                });
-
-                                msg.member
-                                    .send({
-                                        embeds: [kickEmbed],
-                                    })
-                                    .then(() => {
-                                        msg.member.kick({ reason: "Sending links." });
-                                    });
-                            }
-                                break;
-                        }
-                    });
-                }
-            }
-        }
+                            });
+                        };
+                    };
+                };
+            };
+        };
     },
 };
